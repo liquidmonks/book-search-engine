fe7a8fabb867ae436a741337d220b6505cf47d56
Liquid Monks <info@liquidmonks.com>
2023-03-21 17:08:13 -0500

    server/utils/auth.js revises code that exports two functions related to authentication, including one that verifies a JWT token and another that signs a JWT token.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,34 @@
-const jwt = require('jsonwebtoken');
+// Import the jwt library for creating and verifying tokens, and the AuthenticationError class for throwing authentication errors
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+// Export a middleware function that can be used to verify the authenticity of requests
+exports.authMiddleware = (resolver) => {
+  return function (parent, args, context, info) {
+    // Extract the token from the request headers and initialize the user object
+    let token = context.req.headers.authorization;
+    let user = {};
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    // If a token is present, verify it using the JWT_SECRET key and extract the user object from the decoded token
+    if (token) {
+      token = token.split(" ").pop().trim();
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      user = decoded.args;
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+    // If the user object is empty, throw an authentication error
+    if (!user) {
+      throw new AuthenticationError("Authentication required.");
     }
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
+    // Otherwise, continue with the resolver function, passing along the user object in the context
+    return resolver(parent, args, { ...context, user }, info);
+  };
+};
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+// Export a function that signs a token containing the provided username, email, and _id properties, with a 24 hour expiration time
+exports.signToken = ({ username, email, _id }) => {
+  const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  return jwt.sign({ args: payload }, process.env.JWT_SECRET, { expiresIn: "24h" });
 };
