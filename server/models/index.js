8e9a157ff38693083b486883fb75bc0aac45b481
Liquid Monks <info@liquidmonks.com>
2023-03-21 17:18:29 -0500

    server/models/index.js imports the User and Book modules, and exports them as an object.

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,6 @@
+// Import the User and Book modules
 const User = require('./User');
+const Book = require('./Book');
 
-module.exports = { User };
+// Export the User and Book modules as an object
+module.exports = { User, Book };
