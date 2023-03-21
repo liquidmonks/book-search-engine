ea0ede7f9d31e9811b2e2bb58ea608a671b1245f
Liquid Monks <info@liquidmonks.com>
2023-03-21 17:16:25 -0500

    server/models/User.js defines a user schema with required fieldswith user credential encryption. It also adds a virtual field bookCount to the user schema, and creates and exports the User model.

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,10 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+// Import necessary libraries and schemas
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-// import schema from Book.js
-const bookSchema = require('./Book');
+const bookSchema = require("./Book");
 
+// Define the user schema
 const userSchema = new Schema(
   {
     username: {
@@ -15,16 +16,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      match: [/.+@.+\..+/, "Must use a valid email address"],
     },
     password: {
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
     savedBooks: [bookSchema],
   },
-  // set this to use virtual below
   {
     toJSON: {
       virtuals: true,
@@ -32,9 +31,9 @@ const userSchema = new Schema(
   }
 );
 
-// hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+// Encrypt user's password before saving to the database
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -42,16 +41,17 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-// custom method to compare and validate password for logging in
+// Validate user's password during login
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
+// Add a virtual field `bookCount` to the user schema
+userSchema.virtual("bookCount").get(function () {
   return this.savedBooks.length;
 });
 
-const User = model('User', userSchema);
+// Create and export the User model
+const User = model("User", userSchema);
 
 module.exports = User;
