58fdb737e247bddb97394f39d64e264f137fed6e
Liquid Monks <info@liquidmonks.com>
2023-03-26 22:39:53 +0900

    get / -> get *

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const server = new ApolloServer({
 });
 
 app.use(express.static("build"));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
