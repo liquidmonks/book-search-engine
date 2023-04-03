bb489cd410ed4c851a315476bb2ad828082f795c
Liquid Monks <info@liquidmonks.com>
2023-04-03 09:43:03 -0500

    new v

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+// Import the React library for creating components
+import React from "react";
+// Import the ReactDOM library for rendering components to the DOM
+import ReactDOM from "react-dom/client";
+// Import the main App component
+import App from "./App";
+// Import the Bootstrap CSS for styling
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import custom CSS styles
+import "./index.css";
+
+// Create a root DOM node for the React app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the React app within the root DOM node using StrictMode and BrowserRouter
+root.render(
+  // Use React.StrictMode to enable additional checks and warnings during development
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
